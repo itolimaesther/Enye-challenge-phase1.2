6960f24107f680bc8dc5a0b85857c71a465d9e20
itolimaesther <itolimaesther@gmail.com>
2021-01-12 16:08:54 +0100

    added http status code

--- index.js
+++ index.js
@@ -2,27 +2,35 @@
 const express = require('express');
 const axios = require('axios').default;
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World Me!')
+  res.send('Hello World!')
 });
 
 app.get('/api/rates', (req, res) => {
 axios
-  .get("https://api.exchangeratesapi.io/latest?bazze=CZYK&symbols=EUR,GBP,USD")
+  .get("https://api.exchangeratesapi.io/latest?base=CZK&symbols=EUR,GBP,USD")
   .then(function (response) {
     const rawResponse = response.data;
     let results = { };
     results.base = rawResponse.base;
     results.date = rawResponse.date;
     results.rates = rawResponse.rates ;
-    
-    return res.status(200).json({ results:  results});
+
+    res.status(200).json({ results:  results});
+
+    // if(req){
+    //     res.status(200).json({ results:  results});
+    // }else{
+    //     res.status(404).send({message : "This resource was not found"})
+    // }
+
     })
     .catch(function (error) {
       // handle error
-      res.status(404)
+    //   console.log(error);
+    res.status(error.response.status).send(error.response.data).end();
       console.log(error.response);
     });
 
